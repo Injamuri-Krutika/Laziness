6e7ef2b499e30fd7f2605e62ac0a51a229e62824
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-09-01 16:36:53 +0530

    Create file server for accepting the uploaded files

---
+++ file-server.js
@@ -0,0 +1,32 @@
+// server.js
+// Server app to display the webpage
+
+// init project
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
+const app = express();
+const upload = multer({
+  dest: './dsp',
+  fileFilter: (req, file, cb) => {
+    if (path.extension(file.originalname) !== '.pdf') {
+      return cb(new Error('Only pdfs are allowed'));
+    }
+    return cb(null, true);
+  },
+});
+
+app.post('/submit', upload.single('zip'), (request, response) => {
+  const meta = {
+    name: request.file.originalname,
+    mime: request.file.mimetype,
+  };
+  return response.send(meta);
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.FILE_PORT || 7777, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Your app is listening on port ${listener.address().port}`);
+});

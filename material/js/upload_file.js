ec5f1221778b3c1ceb90826f537f713e0fcc8096
Krutika Injamuri <kruteeka.injamuri@gmail.com>
2019-09-05 08:41:01 +0530

    Add validation for file type and also disabling the upload button

--- material/js/upload_file.js
+++ material/js/upload_file.js
@@ -1,3 +1,16 @@
-document.getElementById("uploadBtn").onchange = function() {
-  document.getElementById("uploadFile").value = this.files[0].name;
+/* eslint-disable require-jsdoc */
+function validate() {
+  if (document.getElementById('roll').value !== '' && document.getElementById('set').value !== '' && document.getElementById('uploadFile').value !== '') {
+    console.log(document.getElementById('roll').value, document.getElementById('set').value, document.getElementById('uploadFile').value);
+    document.getElementById('uploadDetails').disabled = false;
+    return;
+  }
+  document.getElementById('uploadDetails').disabled = true;
+}
+
+document.getElementById('uploadBtn').onchange = function () {
+  const filename = this.files[0].name;
+  const extension = filename.split('.').pop();
+  if (extension === 'zip') { document.getElementById('uploadFile').value = filename; }
+  validate();
 };

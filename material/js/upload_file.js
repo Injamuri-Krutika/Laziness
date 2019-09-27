b9b0ede0716bf32321a752f835e116752dd3ac49
Krutika Injamuri <kruteeka.injamuri@gmail.com>
2019-09-27 12:11:17 +0530

    Update the labels

--- material/js/upload_file.js
+++ material/js/upload_file.js
@@ -1,7 +1,6 @@
 /* eslint-disable require-jsdoc */
 function validate() {
-  if (document.getElementById('roll').value !== '' && document.getElementById('set').value !== '' && document.getElementById('uploadFile').value !== '') {
-    console.log(document.getElementById('roll').value, document.getElementById('set').value, document.getElementById('uploadFile').value);
+  if (document.getElementById('roll').value !== '' && document.getElementById('uploadFile').value !== '') {
     document.getElementById('uploadDetails').disabled = false;
     return;
   }
@@ -11,6 +10,6 @@ function validate() {
 document.getElementById('uploadBtn').onchange = function () {
   const filename = this.files[0].name;
   const extension = filename.split('.').pop();
-  if (extension === 'zip') { document.getElementById('uploadFile').value = filename; }
+  if (extension === 'tar') { document.getElementById('uploadFile').value = filename; }
   validate();
 };
